2f54cab03b725b03aaa2b0109c5ed93fd598dd63
Soares17 <goncaloenta2019.20@gmail.com>
2021-11-09 08:12:02 -0100

    Salvar Tudo

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/:nome', (req, res) => {
+  res.send('Hello ' + req.params.nome)
+})
+ 
+const port = 3000
+
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`)
+})
